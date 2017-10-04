3f053bbf9bb43028a74147a2b9dc4b881ae01219
Vasiliy <vasiliy.mv.obzor@gmail.com>
2017-10-04 20:11:07 +0300

    Fix 1.0 change method add strings

--- StagePlay.java
+++ StagePlay.java
@@ -1,8 +1,15 @@
 package com.com.livevasiliy.hometask;
 
-public class StagePlay {
-    public static void main(String[] args) {
-        String[] roles = {"Городничий",
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class StagePlay
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		       String[] roles = {"Городничий",
                 "Аммос Федорович",
                 "Артемий Филиппович"};
 
@@ -16,19 +23,19 @@ public class StagePlay {
                                 "Лука Лукич: Господи боже! еще и с секретным предписаньем!"};
 
         System.out.println(printTextPerRole(roles, textLines));
-    }
-
-    private static String printTextPerRole(String[] roles, String[] textLines) {
-        String stageplaytext = "";
+	}
+	
+	private static String printTextPerRole(String[] roles, String[] textLines) {
+		String stageplaytext = "";
 
         for (String role : roles ) {
-        	stageplaytext.append(role + '\n');
+        	stageplaytext += (role + '\n');
         		for (int i = 0; i < textLines.length; i++ ) {
         			if (textLines[i].startsWith(role + ":")){
-        				stageplaytext.append((i + 1) + ")" + textLines[i].substring(role.length() + 1) + '\n');
+        				stageplaytext += ((i + 1) + ")" + textLines[i].substring(role.length() + 1) + '\n');
         			}
         		}
-        		stageplaytext.append('\n');
+        		stageplaytext +=('\n');
         }
 
         return stageplaytext;
